dc2ea0abb284043a41dfc4bec8bb633044b74802
dongdongx2x2 <kdhdh001@naver.com>
2023-02-04 23:08:59 +0900

    solved 13994

--- algorithm_study/SWEA/13994_newbus/sol.py
+++ algorithm_study/SWEA/13994_newbus/sol.py
@@ -14,33 +14,50 @@ for tc in range(1,T+1):
     arr = []
     for _ in range(1, N+1):
         arr.append(list(map(int, input().split())))
-    print(arr)
+    # print(arr)
 
     max_arr= []
     for i in range(len(arr)):
         max_arr.append(arr[i][1])
         max_arr.append(arr[i][2])
-    print(max(max_arr))
+    # print(max(max_arr))
 
-    count_arr = [0]  * max(max_arr)
-    print(count_arr)
+    count_arr = [0]  * (max(max_arr)+1)
+    # print(count_arr)
 
     for j in range(len(arr)):
-        print(arr[j][0])
-        if arr[j][0] == 1:     # 일반 열차면
+        # print(arr[j][0])
+        if arr[j][0] == 1:     # 일반 버스라면
             for k in range(arr[j][1], arr[j][2]+1): #arr 의 idx [1]부터 [2]값까지 다 1 추가
                 count_arr[k] += 1
     # print(count_arr)
-        elif arr[j][0] == 2: # 급행 버스 라면
-            if arr[j][1] % 2 == 0: #A가 짝수라면
-                for q in range(arr[j][1], arr[j][2]+1):
-                    if q % 2 == 0: # 짝수 인덱스에
-                        count_arr[q] +=1 # 1 추가
-            else: # A가 홀수라면
-                for t in range(arr[j][1], arr[j][2]+1):
-                    if t % 2 == 1:
-                        count_arr[t] += 1
-    print(count_arr)
+        elif arr[j][0] == 2: # 급행 버스라면
+            if arr[j][1] % 2 == 1: #급행버스 A가 홀수라면
+                count_arr[arr[j][1]] += 1 # 양 끝 1 추가
+                count_arr[arr[j][2]] += 1
+                for l in range(arr[j][1]+1, arr[j][2]): # 사이에
+                    if l % 2 == 1:          # 홀수들
+                        count_arr[l] += 1 # 1추가
+            else: # 급행버스 A가 짝수라면
+                count_arr[arr[j][1]] += 1 # 양 끝 1 추가
+                count_arr[arr[j][2]] += 1
+                for m in range(arr[j][1]+1, arr[j][2]): # 사이에 짝수
+                    if m % 2 == 0:
+                        count_arr[m] += 1
+        else: # 광역 급행 버스라면
+            if arr[j][1] % 2 == 1: # 광역버스 A가 홀 수라면
+                count_arr[arr[j][1]] += 1 # 양 끝 1 추가
+                count_arr[arr[j][2]] += 1
+                for n in range(arr[j][1]+1, arr[j][2]): # 사이에 3배수이면서 10배수 아닌거 추가
+                    if n % 3 == 0 and n % 10 != 0:
+                        count_arr[n] += 1
+            else: # 광역 버스A가 짝수라면
+                count_arr[arr[j][1]] += 1 # 양 끝 1 추가
+                count_arr[arr[j][2]] += 1
+                for o in range(arr[j][1]+1, arr[j][2]): # 사이에 4 배수 1추가
+                    if o % 4 == 0:
+                        count_arr[o] +=1
 
-
+    # print(max(count_arr))
+    print(f'#{tc} {max(count_arr)}')
 
