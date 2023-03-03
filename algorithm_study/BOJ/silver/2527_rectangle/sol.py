8abba9bab4535a43dfe6ee6c90bffd8570853335
donghyun <kdhdh001@naver.com>
2023-03-03 16:24:27 +0900

    solve BOJ_2527 silver1

---
+++ algorithm_study/BOJ/silver/2527_rectangle/sol.py
@@ -0,0 +1,31 @@
+import sys
+sys.stdin = open('2527_input.txt')
+
+input = sys.stdin.readline
+
+for _ in range(4):
+    x1, y1, p1, q1, x2, y2, p2, q2 = map(int, input().split())
+
+    if p1 < x2 or p2 < x1 or y1 > q2 or q1 < y2:
+        print('d')
+    elif x1 ==p2 or x2==p1:
+        if q1 == y2 or q2 ==y1:
+            print('c')
+        else:
+            print('b')
+    elif q1 ==y2 or q2 == y1:
+        print('b')
+    else:
+        print('a')
+
+    # if cnt == 0 and cnt2 == 0:
+    #     print('d')
+    # elif cnt == 1 and cnt2 == 1:
+    #     print('c')
+    # elif (cnt == 1 and cnt2 > 1) or (cnt > 1 and cnt2 == 1):
+    #     print('b')
+    # else:
+    #     print('a')
+
+
+
