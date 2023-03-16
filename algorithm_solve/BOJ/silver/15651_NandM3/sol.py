ca669366e35d8d1d365bfa66d21c705e01413a0c
donghyun <kdhdh001@naver.com>
2023-03-16 22:37:30 +0900

    solve BOJ_15651 silver3

---
+++ algorithm_solve/BOJ/silver/15651_NandM3/sol.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('15651_input.txt')
+
+input = sys.stdin.readline
+
+def dfs(n, lst):
+
+    if n == M:
+        ans.append(lst)
+        return
+
+    for j in range(1, N+1):
+        dfs(n+1, lst + [j])
+
+
+N, M = map(int, input().split())
+
+ans = []
+dfs(0, [])
+
+for lst in ans:
+    print(*lst)
