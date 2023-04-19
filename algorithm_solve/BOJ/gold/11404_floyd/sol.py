ab1c4f35fb8451698c1d7533802dd6697f0a8da9
donghyun <kdhdh001@naver.com>
2023-04-20 00:13:42 +0900

    solve BOJ_11404 gold4

---
+++ algorithm_solve/BOJ/gold/11404_floyd/sol.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open('11404_input.txt')
+
+input = sys.stdin.readline
+
+INF = int(1e9)
+
+n = int(input())
+m = int(input())
+
+graph = [[INF] * (n+1) for _ in range(n+1)]
+
+for i in range(n+1):
+    graph[i][i] = 0
+
+for _ in range(m):
+    a, b, c = map(int,input().split())
+    graph[a][b] = min(c, graph[a][b])
+
+for k in range(1, n+1):
+    for x in range(1, n+1):
+        for y in range(1, n+1):
+            graph[x][y] = min(graph[x][y] , graph[x][k] + graph[k][y])
+
+# print(graph)/
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        if graph[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(graph[i][j], end=' ')
+    print()
