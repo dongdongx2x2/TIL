21318f1a505a78c935cf5a4e4d2ddf27c5fe71e3
donghyun <kdhdh001@naver.com>
2023-04-28 14:59:07 +0900

    solve BOJ_1912 silver2

---
+++ algorithm_solve/BOJ/silver/1912_consequencesum/sol.py
@@ -0,0 +1,14 @@
+import sys
+sys.stdin = open('1912_input.txt')
+
+input = sys.stdin.readline
+
+n = int(input())
+
+lst = list(map(int,input().split()))
+
+for i in range(1, n):
+
+    lst[i] = max(lst[i], lst[i]+lst[i-1])
+
+print(max(lst))
