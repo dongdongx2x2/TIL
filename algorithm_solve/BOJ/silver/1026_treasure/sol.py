1558cc82921bc473c0cd7f015c5028cc4ac97e35
김동현 <kdhdh001@naver.com>
2023-09-04 13:59:49 +0900

    solve BOJ_1026 silver4

---
+++ algorithm_solve/BOJ/silver/1026_treasure/sol.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('1026_input.txt')
+
+input = sys.stdin.readline
+
+N = int(input())
+A = list(map(int,input().split()))
+B = list(map(int,input().split()))
+
+A.sort()
+B.sort(reverse=True)
+ans = 0
+
+for i in range(N):
+    ans += A[i] * B[i]
+print(ans)
+
