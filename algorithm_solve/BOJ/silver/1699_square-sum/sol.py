bc577baa10fb5d4f707ffe28472bf917937bee4f
donghyun <kdhdh001@naver.com>
2024-01-27 16:13:12 +0900

    solve BOJ_1699 silver2

---
+++ algorithm_solve/BOJ/silver/1699_square-sum/sol.py
@@ -0,0 +1,16 @@
+import sys
+sys.stdin = open('1699_input.txt')
+
+input = sys.stdin.readline
+
+n = int(input())
+dp = [i for i in range(n + 1)]
+
+for i in range(1, n + 1):
+    for j in range(1, i):
+        if j * j > i:
+            break
+
+        if dp[i] > dp[i-j*j] + 1:
+            dp[i] = dp[i-j*j] + 1
+print(dp[n])
