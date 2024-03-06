deab21780dc57a12478c8ca095cb445f57077519
donghyun <kdhdh001@naver.com>
2024-03-06 09:51:20 +0900

    solve BOJ_1300 gold1

---
+++ algorithm_solve/BOJ/gold/1300_Knum/sol.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin = open('1300_input.txt')
+
+input = sys.stdin.readline
+
+n = int(input())
+k = int(input())
+
+start, end = 1, k
+
+while start <= end:
+    mid = (start + end) // 2
+
+    tem = 0
+
+    for i in range(1,  n + 1):
+        tem += min(mid // i, n)
+
+    if tem >= k:
+        ans = mid
+        end = mid - 1
+
+    else:
+        start = mid + 1
+print(ans)
