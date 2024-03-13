ebd5703c6c4a740eec528afb37f252d91898841c
donghyun <kdhdh001@naver.com>
2024-03-13 10:28:34 +0900

    solve BOJ_19539 gold5

---
+++ algorithm_solve/BOJ/gold/19539_appleTree/sol.py
@@ -0,0 +1,21 @@
+import sys
+sys.stdin = open('19539_input.txt')
+
+input = sys.stdin.readline
+
+n = int(input())
+lst = list(map(int, input().split()))
+
+cnt = 0
+
+if sum(lst) % 3 == 0:
+    for i in lst:
+        cnt += i // 2
+
+    if cnt >= (sum(lst) // 3):
+        print("YES")
+    else:
+        print("NO")
+
+else:
+    print("NO")
