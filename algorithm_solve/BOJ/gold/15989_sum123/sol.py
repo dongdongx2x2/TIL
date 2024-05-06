6da2eba701d3ad549cfb5df6de9c9e97df8efc57
donghyun <kdhdh001@naver.com>
2024-05-06 09:37:36 +0900

    solve BOJ_15989 gold5

---
+++ algorithm_solve/BOJ/gold/15989_sum123/sol.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('15989_input.txt')
+
+input = sys.stdin.readline
+
+dp = [1] * 10001
+
+for i in range(2, 10001):
+    dp[i] += dp[i-2]
+
+for i in range(3, 10001):
+    dp [i] += dp[i-3]
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    print(dp[n])
