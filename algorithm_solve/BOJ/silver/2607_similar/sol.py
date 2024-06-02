a39a7a353e6e63afccd7d10db8de028c7886f581
donghyun <kdhdh001@naver.com>
2024-06-02 15:23:30 +0900

    solve BOJ_2607 silver2

---
+++ algorithm_solve/BOJ/silver/2607_similar/sol.py
@@ -0,0 +1,25 @@
+import sys
+sys.stdin = open('2607_input.txt')
+
+input = sys.stdin.readline
+
+n = int(input())
+target = list(input())
+
+ans = 0
+
+for _ in range(n-1):
+    compare = target[:]
+    word = input()
+    cnt = 0
+
+    for w in word:
+        if w in compare:
+            compare.remove(w)
+        else:
+            cnt += 1
+
+    if cnt < 2 and len(compare) < 2:
+        ans += 1
+
+print(ans)
