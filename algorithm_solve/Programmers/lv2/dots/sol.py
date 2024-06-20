57ffb52a86ccb4af41d107f55e21d8acd426fc48
donghyun <kdhdh001@naver.com>
2024-06-20 10:23:16 +0900

    solve programmers_dots lv2

---
+++ algorithm_solve/Programmers/lv2/dots/sol.py
@@ -0,0 +1,9 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/140107
+def solution(k, d):
+    answer = 0
+    
+    for x in range(0,d+1,k) :
+        max_y = int((d**2 - x**2)**0.5)
+        answer += (max_y // k) +1
+    
+    return answer
