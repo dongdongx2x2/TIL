e2dd500645755ef04ecd53fb107e3c040d7eafca
donghyun <kdhdh001@naver.com>
2024-09-19 09:37:41 +0900

    solve programmers_valid-bracket lv2

---
+++ algorithm_solve/Programmers/lv2/valid-bracket/sol.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/12909
+def solution(s):
+    stack = []
+    
+    for char in s:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack:
+                return False
+            stack.pop()
+    
+    return len(stack) == 0
