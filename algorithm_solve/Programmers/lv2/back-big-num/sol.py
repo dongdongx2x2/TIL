e85c12976a4cbe1d584ada405100096d090e8683
donghyun <kdhdh001@naver.com>
2024-10-28 00:01:21 +0900

    solve programmers back-big-num lv2

---
+++ algorithm_solve/Programmers/lv2/back-big-num/sol.py
@@ -0,0 +1,17 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/154539
+
+def solution(numbers):
+    n = len(numbers)
+    answer = [-1] * n
+    stack = []
+
+    for i in range(n - 1, -1, -1):
+        while stack and stack[-1] <= numbers[i]:
+            stack.pop()
+        
+        if stack:
+            answer[i] = stack[-1]
+        
+        stack.append(numbers[i])
+
+    return answer
